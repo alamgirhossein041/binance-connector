f0dfc8c7ccc8d515995f72a3295836e8f702f381
Gogs <gogs@fake.local>
2022-11-12 10:18:50 +0000

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+class Futures {
+
+    restEndPointTestNet = "https://testnet.binancefuture.com"
+    WsEndPointTestNet   = "wss://stream.binancefuture.com"
+    baseEndPoint        = "https://fapi.binance.com"
+    wsEndPoint          = ""
+
+    // application/x-www-form-urlencoded
+    // X-MBX-USED-WEIGHT-(intervalNum)(intervalLetter) => X-MBX-USED-WEIGHT-1m => IP
+    // X-MBX-ORDER-COUNT-(intervalNum)(intervalLetter) => X-MBX-ORDER-COUNT-1m => Order
+
+    constructor(options) {
+
+    }
+
+
+}
