11a09aaca20bc80004e8ea69a49491d6902484ef
M.Hasan Jabbari <39640448+mhasanjb@users.noreply.github.com>
2022-11-24 10:11:20 +0000

    spot

--- core/spot.js
+++ core/spot.js
@@ -63,19 +63,387 @@ export class Spot {
         this.http = new Http(OPTIONS)
     }
 
+    // ########################################### Wallet Endpoints
     // ########### Public
     /**
      * @param {SpotJustRecvWindow} [params]
      */
-    async exchangeInfo(params) {
-        return await this.http.publicGET("/api/v3/exchangeInfo", params)
+    async systemStatus(params) {
+        return await this.http.publicGET("/sapi/v1/system/status", params)
     }
     
-    async assetDetail(params) {
+    // ########### Private
+    /**
+     * @param {} [params]
+     */
+    async capitalConfigGetAll(params) {
+        return await this.http.privateGET("/sapi/v1/capital/config/getall", params)
+    }
+
+
+    /**
+     * @param {} [params]
+     */
+    async accountSnapshot(params) {
+        return await this.http.privateGET("/sapi/v1/accountSnapshot", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async accountDisableFastWithdrawSwitch(params) {
+        return await this.http.privateGET("/sapi/v1/account/disableFastWithdrawSwitch", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async newAccountEnableFastWithdrawSwitch(params) {
+        return await this.http.privatePOST("/sapi/v1/account/enableFastWithdrawSwitch", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async newCapitalWithdrawApply(params) {
+        return await this.http.privatePOST("/sapi/v1/capital/withdraw/apply", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async capitalDepositHisrec(params) {
+        return await this.http.privateGET("/sapi/v1/capital/deposit/hisrec", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async capitalWithdrawHistory(params) {
+        return await this.http.privateGET("/sapi/v1/capital/withdraw/history", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async capitalDepositAddress(params) {
+        return await this.http.privateGET("/sapi/v1/capital/deposit/address", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async accountStatus(params) {
+        return await this.http.privateGET("/sapi/v1/account/status", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async accountApiTradingStatus(params) {
+        return await this.http.privateGET("/sapi/v1/account/apiTradingStatus", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async assetDribblet(params) {
+        return await this.http.privateGET("/sapi/v1/asset/dribblet", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async newAssetDustBTC(params) {
+        return await this.http.privatePOST("/sapi/v1/asset/dust-btc", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async newAssetDust(params) {
+        return await this.http.privatePOST("/sapi/v1/asset/dust", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async assetAssetDividend(params) {
+        return await this.http.privateGET("/sapi/v1/asset/assetDividend", params)
+    }
+    
+    /**
+     * @param {} [params]
+     */
+    async assetAssetDetail(params) {
         return await this.http.privateGET("/sapi/v1/asset/assetDetail", params)
     }
 
-    async test(params) {
-        return await this.http.privateGET("/sapi/v1/capital/config/getall", params)
+    /**
+     * @param {} [params]
+     */
+    async assetTradeFee(params) {
+        return await this.http.privateGET("/sapi/v1/asset/tradeFee", params)
     }
+
+    /**
+     * @param {} [params]
+     */
+    async newAssetTransfer(params) {
+        return await this.http.privatePOST("/sapi/v1/asset/transfer", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async assetTransfer(params) {
+        return await this.http.privateGET("/sapi/v1/asset/transfer", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async assetGetFundingAsset(params) {
+        return await this.http.privatePOST("/sapi/v1/asset/get-funding-asset", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async assetGetUserAsset(params) {
+        return await this.http.privatePOST("/sapi/v3/asset/getUserAsset", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async newAssetConvertTransfer(params) {
+        return await this.http.privatePOST("/sapi/v1/asset/convert-transfer", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async assetConvertTransferQueryByPage(params) {
+        return await this.http.privateGET("/sapi/v1/asset/convert-transfer/queryByPage", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async assetLedgerTransferCloudMiningQueryByPage(params) {
+        return await this.http.privateGET("/sapi/v1/asset/ledger-transfer/cloud-mining/queryByPage", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async accountApiRestrictions(params) {
+        return await this.http.privateGET("/sapi/v1/account/apiRestrictions", params)
+    }
+    
+    // ########################################### Sub-Account Endpoints
+    // ########### Public
+    // ########### Private
+    /**
+     * @param {} [params]
+     */
+    async newSubAccountVirtualSubAccount(params) {
+        return await this.http.privatePOST("/sapi/v1/sub-account/virtualSubAccount", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+ 
+    /**
+     * @param {} [params]
+     */
+    async f(params) {
+        return await this.http.privateGET("", params)
+    }
+    
+
+
+    // ########################################### Market Data Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Spot Account/Trade
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Margin Account/Trade
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Savings Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Stacking Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Mining Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Futures
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Futures Algo Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Portfolio Margin Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### BLVT Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### BSwap Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Fiat Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### C2C Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### VIP Loans Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Crypto Loans Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Pay Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Convert Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Rebate Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### NFT Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Binance Code Endpoints
+    // ########### Public
+    // ########### Private
+
 }
